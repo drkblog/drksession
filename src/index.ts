ab885a219b176a50a46359bd38b38d70bdc1acdb
Leandro <drkbugs@gmail.com>
2022-01-30 22:13:04 -0300

    Release 1.0.10

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { HTTP_CODE, SessionManager } from "./drksession";
+export { HTTP_CODE, SessionManager, SessionManagerConfiguration } from "./drksession";
 export { getCookies, createRedirect, createRedirectWithCookies } from "./drksession";
 export { KvAdapter, DefaultKvAdapter, MockKvAdapter } from "./kvadapter";
